52d13033c44023f75109ab3610b930a36df36302
drreineke13 <123852092+drreineke13@users.noreply.github.com>
2023-01-28 22:54:27 -0500

    uploaded main.hpp from server

---
+++ main.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <dpp/dpp.h>
+#include <iomanip>
+#include <sstream>
+#include <errno.h>
+
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <ogg/ogg.h>
+#include <opus/opusfile.h>
+
+#include "dlHandler.hpp"
